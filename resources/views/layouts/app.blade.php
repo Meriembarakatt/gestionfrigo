17ebe221ad51071fe032247a04e3faccf8d88900
m.barakat <meriembarakat30@gmail.com>
2024-05-22 17:07:05 +0200

    button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,6 +43,7 @@
     </div>
 @endif
     <div class="container mx-auto py-8">
+        <aside><button><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></button></aside>
         <h1 class="text-3xl font-semibold mb-4">Bienvenue sur notre site</h1>
         
         <div class="photo-container">
